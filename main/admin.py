27538c9241318b37d44376e61787fb27a397377d
Alex Heist <alexheistdev@gmail.com>
2020-05-24 12:07:06 -0600

    models & admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.AboutImage)
+class AboutImageAdmin(admin.ModelAdmin):
+    list_display = ("description", "in_use")
+
+
+@admin.register(models.AboutText)
+class AboutTextAdmin(admin.ModelAdmin):
+    list_display = ("content", "in_use")
+
+
+@admin.register(models.AboutVideo)
+class AboutVideoAdmin(admin.ModelAdmin):
+    list_display = ("description", "in_use")
+
+
+@admin.register(models.Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ("text", "link", "in_use")
+
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "venue",
+        "date",
+    )
+    date_heirarchy = "date"
+
+
+@admin.register(models.Inquiry)
+class InquiryAdmin(admin.ModelAdmin):
+    list_display = ("email", "name", "message", "received", "seen", "responded_to")
+    date_heirarchy = "received"
+
+
+@admin.register(models.Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = [
+        "description",
+    ]
+
+
+@admin.register(models.Music)
+class MusicAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+    ]
+
+
+@admin.register(models.News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ("title", "published")
+    date_heirarchy = "published"
+
+
+@admin.register(models.Press)
+class PressAdmin(admin.ModelAdmin):
+    list_display = ("source", "link")
+
+
+admin.site.site_header = "Welcome Back"
+admin.site.site_title = "Sick Wish"
+admin.site.index_title = "Admin"
--- main/models.py
+++ main/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from . import choices
+
+
+class AboutImage(models.Model):
+    description = models.CharField(max_length=255)
+    image = models.ImageField()
+    in_use = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "About Page Images"
+
+
+class AboutText(models.Model):
+    content = models.TextField()
+    in_use = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "About Page Text"
+
+
+class AboutVideo(models.Model):
+    description = models.CharField(max_length=255)
+    embed_html = models.TextField()
+    in_use = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "About Page Videos"
+
+
+class Banner(models.Model):
+    text = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    in_use = models.BooleanField(default=False)
+
+
+class Event(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    venue = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2, choices=choices.STATES, default="ID")
+    cover = models.DecimalField(decimal_places=2, max_digits=8, null=True, blank=True)
+    age_restrictions = models.CharField(
+        max_length=2, choices=choices.RESTRICTIONS, default="AA"
+    )
+
+
+class Inquiry(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    received = models.DateTimeField(default=timezone.now)
+    seen = models.BooleanField(default=False)
+    responded_to = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Inquiries"
+
+
+class Media(models.Model):
+    description = models.CharField(max_length=255)
+    image = models.ImageField()
+
+    class Meta:
+        verbose_name_plural = "Media Page Images"
+
+
+class Music(models.Model):
+    title = models.CharField(max_length=255)
+    embed_html = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Embedded Albums"
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published = models.DateField()
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name_plural = "News Posts"
+
+
+class Press(models.Model):
+    source = models.CharField(max_length=255)
+    image = models.ImageField()
+    content = models.TextField()
+    link = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Press"
