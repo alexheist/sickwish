53928db8be9776865bc336aa412ca4f339166789
Alex Heist <alexheistdev@gmail.com>
2020-06-14 16:50:58 -0600

    mobile templates (excluding media & about)

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("news/", views.news),
+    path("news/<str:slug>/", views.news_article),
+    path("tour/", views.tour),
+    path("music/", views.music),
+    path("media/", views.media),
+    path("about/", views.about),
 ]
--- main/views.py
+++ main/views.py
@@ -10,7 +10,7 @@ def index(request):
     news = models.News.objects.filter(published__lte=current_date).order_by(
         "-published"
     )[:3]
-    events = models.Event.objects.all().order_by("-date")
+    events = models.Event.objects.all().order_by("-date")[:3]
     if request.is_ajax():
         form = forms.LeadForm(request.POST)
         if form.is_valid():
@@ -18,3 +18,34 @@ def index(request):
             return http.HttpResponse(status=200)
         return http.HttpResponse(status=400)
     return render(request, "main/index.html", {"news": news, "events": events})
+
+
+def news(request):
+    current_date = timezone.localtime().date()
+    news = models.News.objects.filter(published__lte=current_date).order_by(
+        "-published"
+    )
+    return render(request, "main/news.html", {"news": news})
+
+
+def news_article(request, slug):
+    article = models.News.objects.get(slug=slug)
+    return render(request, "main/news-article.html", {"article": article})
+
+
+def tour(request):
+    events = models.Event.objects.all().order_by("-date")
+    return render(request, "main/tour.html", {"events": events})
+
+
+def music(request):
+    music = models.Music.objects.all()
+    return render(request, "main/music.html", {"music": music})
+
+
+def media(request):
+    return render(request, "main/media.html", {})
+
+
+def about(request):
+    return render(request, "main/about.html", {})
