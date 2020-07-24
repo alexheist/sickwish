4e11b2c2448c522916822f53898e3772e45b5c57
Alex Heist <alexheistdev@gmail.com>
2020-07-23 18:14:52 -0600

    banner

--- main/views.py
+++ main/views.py
@@ -13,6 +13,7 @@ def get_or_none(model, *args, **kwargs):
 
 
 def index(request):
+    banner = get_or_none(models.Banner, in_use=True)
     current_date = timezone.localtime().date()
     news = models.News.objects.filter(published__lte=current_date).order_by(
         "-published"
@@ -25,41 +26,53 @@ def index(request):
             form.save()
             return http.HttpResponse(status=200)
         return http.HttpResponse(status=400)
-    return render(request, "main/index.html", {"news": news, "events": events})
+    return render(
+        request, "main/index.html", {"banner": banner, "news": news, "events": events}
+    )
 
 
 def news(request):
+    banner = get_or_none(models.Banner, in_use=True)
     current_date = timezone.localtime().date()
     news = models.News.objects.filter(published__lte=current_date).order_by(
         "-published"
     )
-    return render(request, "main/news.html", {"news": news})
+    return render(request, "main/news.html", {"banner": banner, "news": news})
 
 
 def news_article(request, slug):
+    banner = get_or_none(models.Banner, in_use=True)
     article = models.News.objects.get(slug=slug)
-    return render(request, "main/news-article.html", {"article": article})
+    return render(
+        request, "main/news-article.html", {"banner": banner, "article": article}
+    )
 
 
 def tour(request):
+    banner = get_or_none(models.Banner, in_use=True)
     events = models.Event.objects.all().order_by("-date")
-    return render(request, "main/tour.html", {"events": events})
+    return render(request, "main/tour.html", {"banner": banner, "events": events})
 
 
 def music(request):
+    banner = get_or_none(models.Banner, in_use=True)
     music = models.Music.objects.all()
-    return render(request, "main/music.html", {"music": music})
+    return render(request, "main/music.html", {"banner": banner, "music": music})
 
 
 def media(request):
+    banner = get_or_none(models.Banner, in_use=True)
     media = models.Media.objects.all()
-    return render(request, "main/media.html", {"media": media})
+    return render(request, "main/media.html", {"banner": banner, "media": media})
 
 
 def about(request):
+    banner = get_or_none(models.Banner, in_use=True)
     image = get_or_none(models.AboutImage, in_use=True)
     text = get_or_none(models.AboutText, in_use=True)
     video = get_or_none(models.AboutVideo, in_use=True)
     return render(
-        request, "main/about.html", {"image": image, "text": text, "video": video}
+        request,
+        "main/about.html",
+        {"banner": banner, "image": image, "text": text, "video": video},
     )
